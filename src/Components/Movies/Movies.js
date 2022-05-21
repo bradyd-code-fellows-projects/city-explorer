7db7876da211b8b69d06d5353206498fb9ca4efa
Brady <brady.davenport@pm.me>
2022-05-20 22:21:54 -0600

    started work on getting posterpaths

--- src/Components/Movies/Movies.js
+++ src/Components/Movies/Movies.js
@@ -8,10 +8,16 @@ class Movies extends React.Component {
     let moviesInfo = this.props.movies;
     let moviesArr = moviesInfo.map((movie, idx) => {
       return <ListGroup key={idx} id="moviesList">
+        {movie.posterpath &&
+          <Image
+            src={movie.posterpath}
+            alt={movie.title}
+          />
+        }
+
         <ListGroup.Item>{movie.title}</ListGroup.Item>
         <ListGroup.Item>{movie.overview}</ListGroup.Item>
         <ListGroup.Item>{movie.releasedOn}</ListGroup.Item>
-        <Image src={movie.posterpath}></Image>
       </ListGroup>
     })
     return moviesArr;

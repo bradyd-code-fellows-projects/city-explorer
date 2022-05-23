f86da7edf7c85824b587ceab8069204721d533e7
Brady <brady.davenport@pm.me>
2022-05-22 21:50:01 -0600

    trying to get weather day modal to work

--- src/Components/Weather/WeatherDay.js
+++ src/Components/Weather/WeatherDay.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import '../Weather/Weather.css'
-import { Carousel, Modal, Button } from 'react-bootstrap';
+import { Modal, Button } from 'react-bootstrap';
 import '../Weather/Weather.js'
 
 class WeatherDay extends React.Component {
 
   render() {
 
-    let weatherInfo = this.props.weather;
-
     return (
       <>
-        <Modal>
-          <Modal.Header>
-            <Modal.Title>Forecast for {this.props.date}</Modal.Title>
+        <Modal
+          show={this.props.weatherModalIsDisplaying}
+          onHide={this.props.closeWeatherModalHandler}>
+          <Modal.Header closeButton>
+            <Modal.Title>Forecast for {this.props.SelectedDay.date}</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <img src="" alt=""/>
-            <p></p>
+            <img src={this.props.SelectedDay.icon} alt={this.props.SelectedDay.description} />
+            <p>Low: {this.props.SelectedDay.low}</p>
+            <p>High: {this.props.SelectedDay.high}</p>
+            <p>Forecast: {this.props.SelectedDay.description}</p>
           </Modal.Body>
           <Modal.Footer>
-            <Button onClick={}>Close</Button>
+            <Button onClick={this.props.closeWeatherModalHandler}>Close</Button>
           </Modal.Footer>
         </Modal>
       </>
